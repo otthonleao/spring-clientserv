752a8102adbf231797b8835881375f1b210740c5
otthonleao <otthonleao@hotmail.com>
2024-08-20 14:22:13 -0400

    Initial commit

---
+++ src/main/java/dev/otthon/clientserv/model/Cliente.java
@@ -0,0 +1,2 @@
+package dev.otthon.clientserv.model;public class Cliente {
+}
---
+++ src/test/java/dev/otthon/clientserv/SpringClientserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.otthon.clientservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringClientserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

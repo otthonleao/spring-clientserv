a3c5d4ee3f564548dfaa086647ab58d3b2b9c828
otthonleao <otthonleao@hotmail.com>
2024-08-21 01:34:39 -0400

    feat(Clientes): add Model, Repository and Controller POST, GET/id, DELETE/id and PUT/id

---
+++ src/main/java/dev/otthon/clientserv/controller/ClienteController.java
@@ -0,0 +1,54 @@
+package dev.otthon.clientserv.controller;
+
+import dev.otthon.clientserv.model.Cliente;
+import dev.otthon.clientserv.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+@RequestMapping("/api/clientes")
+public class ClienteController {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente cadastrar(@RequestBody Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+
+    @GetMapping("{id}")
+    public Cliente buscarPorId(@PathVariable Long id) {
+        return clienteRepository.findById(id)
+                .orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND)
+        );
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletar(@PathVariable Long id) {
+        clienteRepository.findById(id)
+                .map(cliente -> {
+                    clienteRepository.delete(cliente);
+                    return Void.TYPE;
+                })
+                .orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @PutMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void atualizar(@PathVariable Long id, @RequestBody Cliente clienteAtualizado) {
+        clienteRepository.findById(id)
+                .map( cliente -> {
+//                    clienteAtualizado.setId(cliente.getId());
+                    clienteAtualizado.setNome(clienteAtualizado.getNome());
+                    clienteAtualizado.setCpf(clienteAtualizado.getCpf());
+                    return clienteRepository.save(clienteAtualizado);
+                })
+                .orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+}
--- src/main/java/dev/otthon/clientserv/model/Cliente.java
+++ src/main/java/dev/otthon/clientserv/model/Cliente.java
@@ -1,2 +1,42 @@
-package dev.otthon.clientserv.model;public class Cliente {
+package dev.otthon.clientserv.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty
+    @Column(name = "NOME", nullable = false, length = 150)
+    private String nome;
+
+    @NotNull
+    @Column(name = "CPF", nullable = false, length = 11, unique = true)
+    private String cpf;
+
+    @Column(name = "DATA_CADASTRO", updatable = false)
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate dataCadastro;
+
+    @PrePersist
+    public void prePersist() {
+        setDataCadastro(LocalDate.now());
+    }
+
 }
